09502dcaa9b5e2d38c1501154d89759422081d3d
astern <alena7690@gmail.com>
2018-02-18 20:18:35 -0600

    function to import census data to postgresql

--- census_to_sql.py
+++ census_to_sql.py
@@ -2,18 +2,18 @@ def census_to_sql(year):
 	'''
 	takes year as a parameter and returns SQL queries to create table in po
 	'''
-	table_name = census_ + str(year)
-	create_table_str =  "CREATE TABLE " + table_name + " (geo_id varchar(50), geo_name varchar(50), ind_id varchar(10), life_exp float8, year int);"
+	table_name = "census_" + str(year)
+	create_table_str =  "CREATE TABLE " + table_name + " (GISJOIN varchar (50), YEAR int, BLOCKA varchar(10), BLOCK_GRPA int, TRACTA int, C_CITYA varchar(10), COUNTY varchar(10), MSA_CMSAA varchar(10), STATE varchar(10), Total_Pop int, PCT_WHITE float8, PCT_BLACK float8, PCT_OTHER float8, TOTAL_UNITS int, Median int, PCT_OCCUPIED float8, PCT_VACANT float8, PCT_OWN_OCC float8, PCT_RENT_OCC float8);"
+	copy_str = "COPY " + table_name + "(GISJOIN, year, BLOCKA, BLOCK_GRPA, TRACTA, C_CITYA, COUNTY, MSA_CMSAA, STATE, Total_Pop, PCT_WHITE, PCT_BLACK, PCT_OTHER, TOTAL_UNITS, Median, PCT_OCCUPIED, PCT_VACANT, PCT_OWN_OCC, PCT_RENT_OCC)"
+	from_str = " FROM " + "'/Users/alenastern/Documents/Win2018/CAPP30122/CAPP-Win18-Project/data/census_files_" + str(year)+"/"+str(year)+"_census_data.csv' WITH DELIMITER AS '|' CSV HEADER;"
 
-	copy_str = " COPY " + table_name + "(geo_id, geo_name, ind_id, life_exp, year)"
-	from_str = " FROM " + "'/Users/alenastern/Documents/Win2018/CAPP30122/CAPP-Win18-Project/data/census_" + year + ".csv' WITH CSV HEADER;"
+	return create_table_str,  copy_str + from_str
 
-return create_table_str + copy_str + from_str
 
-s = postgres.connect()
-c = s.cursor()
+#s = postgres.connect()
+#c = s.cursor()
 
-year_list = [1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
-for year in year_list:
-	query = census_to_sql(year)
-	c.execute(query)
+#year_list = [1940, 1950, 1960, 1970, 1980, 1990, 2000, 2000]
+#for year in year_list:
+#	query = census_to_sql(year)
+#	c.execute(query)
