6bc3f5e6ff3073cf2c8cc0669a714aa1a796b6c5
astern <alena7690@gmail.com>
2018-02-22 23:46:02 -0600

    export geojson

--- shp2pgsql.py
+++ shp2pgsql.py
@@ -4,6 +4,13 @@ shp2pgsql -s 4326 HOLC_chicago.shp public.redline | psql -d capp30122
 
 #import CHA places data
 ogr2ogr -f PostgreSQL \
-PG:"host='localhost' user='postgres' port='5432' \
+PG:"host='localhost' user='alenastern' port='5432' \
 dbname='capp30122' password=''" \
 CHA_places.json 
+
+#life expectancy by neighborhood
+ogr2ogr -f GeoJSON -t_srs EPSG:4326 le_2010.geojson \
+PG:"host='localhost' dbname='capp30122' user='alenastern' password='' port='5432'" \
+-sql "SELECT life_exp, wkb_geometry  \
+FROM life_exp JOIN cha_places_test ON life_exp.geo_name = cha_places_test.name \
+WHERE year = 2010"
