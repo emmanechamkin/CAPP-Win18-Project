cfa7f560ddb239f76f6bf4455c5b21b272e2a693
astern <alena7690@gmail.com>
2018-03-02 23:45:24 -0600

    spatial join progress

--- shp2pgsql.py
+++ shp2pgsql.py
@@ -40,17 +40,18 @@ shp2pgsql -s 102003:4326 IL_block_1990.shp public.census_1990_shp | psql -d capp
 shp2pgsql -s 102003:4326 IL_block_2000.shp public.census_2000_shp | psql -d capp30122
 
 
+#test export redline polygon
+ogr2ogr -f "GeoJSON" C:\Users\alenastern\Desktop\redline_poly_a.geojson  \
+PG:"host='localhost' dbname='capp30122' user='alenastern' password='' port='5432'" \
+-sql "SELECT * from redline_poly;" -t_srs EPSG:4326 
+
+
 #geoJSON of percent black holc grade in 1990
 ogr2ogr -f GeoJSON -t_srs EPSG:4326 pctblack_holc_1990.geojson \
 PG:"host='localhost' dbname='capp30122' user='alenastern' password='' port='5432'" 
 -sql 
 
-CREATE TABLE Census_Black_1990 (pct_black float8, geom geometry);
-
-SELECT redline.gid, avg(c.pct_black) AS pct_black, redline.geom 
-FROM (SELECT c90.pct_black, c90s.geom FROM census_1990 AS c90 JOIN census_1990_shp AS c90s ON c90.gisjoin = c90s.gisjoin) AS c 
-JOIN redline on ST_Intersects(ST_GeomFromEWKB(c.geom), redline.geom) 
-GROUP BY redline.gid;
+C
 
 SELECT 
 
