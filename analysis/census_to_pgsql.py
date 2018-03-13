d2bd436a8e88d7f68c419d8159be4857f3a68549
Claire Herdeman <cherdeman@uchicago.edu>
2018-03-13 11:56:14 -0500

    adding analysis folder

---
+++ analysis/census_to_pgsql.py
@@ -0,0 +1,126 @@
+import psycopg2 
+import os 
+
+FILE_PATH = "'/Users/alenastern/Documents/Win2018/CAPP30122/CAPP-Win18-Project/data/census_data/"
+
+def census_to_sql(year, file_path = FILE_PATH):
+	'''
+	Imports census data in csv to table in sql database.
+
+	Inputs:
+		year (int): year of census data
+		file_path (str): file path where census data stored 
+
+	Returns:
+		create_table_str (str): string containing quary to create table in sql 
+			database
+		copy_str + from_str (str): string containing query to copy data from
+			census csv file into sql table
+ 
+	'''
+	table_name = "census_" + str(year)
+	#account for variations in fields across census years
+	if year < 1951:
+		median_create = " Median float8, "
+		median_copy = " Median,"
+		locality_create = " COUNTY varchar(20),"
+		locality_copy = "COUNTY,"
+		geography_create = ""
+		geography_copy = ""
+		delim = ","
+	elif year < 1981:
+		median_create = ""
+		median_copy = ""
+		locality_create = " COUNTY varchar(20),"
+		locality_copy = "COUNTY,"
+		geography_create = ""
+		geography_copy = ""
+		delim = ","
+	else:
+		median_create = " Median float8, "
+		median_copy = " Median,"
+		locality_create = ''' C_CITYA varchar(10), COUNTY varchar(20), 
+			MSA_CMSAA varchar(10),'''
+		locality_copy = "C_CITYA, COUNTY, MSA_CMSAA,"
+		geography_create = "BLOCKA varchar(10), BLOCK_GRPA int,"
+		geography_copy = "BLOCKA, BLOCK_GRPA,"
+		delim = "|"
+
+
+	create_table_str =  ("CREATE TABLE " + table_name +
+	"(GISJOIN varchar (50), YEAR int, TRACTA varchar(10)," + geography_create + 
+	locality_create +  ''' STATE varchar(10), Total_Pop float8, PCT_WHITE float8, 
+	PCT_BLACK float8, PCT_OTHER float8, TOTAL_UNITS float8,'''+ median_create 
+	+ '''PCT_OCCUPIED float8, PCT_VACANT float8, PCT_OWN_OCC float8,
+	PCT_RENT_OCC float8, PRIMARY KEY (GISJOIN));''')
+	copy_str = ("COPY " + table_name + "(GISJOIN, year, TRACTA, " + geography_copy +
+	locality_copy + " STATE, Total_Pop, PCT_WHITE, PCT_BLACK, PCT_OTHER, TOTAL_UNITS,"
+	+ median_copy + " PCT_OCCUPIED, PCT_VACANT, PCT_OWN_OCC, PCT_RENT_OCC)")
+	from_str = (" FROM " + FILE_PATH +str(year)+
+	"_census_data.csv' WITH DELIMITER AS '{}' CSV HEADER NULL AS '';").format(delim)
+
+	return create_table_str,  copy_str + from_str
+
+
+def normalized_median(year):
+	'''
+	Performs calculations to generate normalized median variable for the census
+	table in a given year. Normalized median is defined as follows:
+
+		(median - mean(median))/std dev(median)
+
+	Inputs:
+		year (int): year of census data
+	Returns:
+		add_col (str): query to add column to census data table
+		insert_table (str): query to insert gisjoin (unique ID) and standardized 
+			median into intermediate table
+		update_table (str): query to update census table for given year with
+			standardized median data from intermediate table
+
+	'''
+    add_col = "ALTER TABLE census_{} add column norm_med float8;".format(year)
+    insert_table = '''insert into intermed(gisjoin, norm_med) select gisjoin, 
+    	(median - (Select avg(median) from census_{} 
+    	where median != 'NaN'))/(select stddev(median) from census_{} 
+    	where median != 'NaN') as norm_med from census_{};'''.format(year, year, year)
+    update_table = '''update census_{} set norm_med= intermed.norm_med from 
+    	intermed where census_{}.gisjoin = intermed.gisjoin;'''.format(year,year)
+
+
+    return (add_col, insert_table, update_table)
+
+
+conn = psycopg2.connect(database="capp30122", user="alenastern", password='', 
+	host="localhost", port="5432")
+c = conn.cursor()
+
+year_list = [1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010]
+
+for year in year_list:
+	# Create database table for census data for given year
+	table, copy = census_to_sql(year)
+	c.execute(table)
+	c.execute(copy)
+	# Import census geographic unit shapefile for given year
+	shp_name = "census_shapefiles/IL_block_{}.shp".format(year)
+	table_name = "public.census_{}_shp".format(year)
+	os.system("shp2pgsql -s 102003:4326 {} {} | psql -d capp30122".format(shp_name, table_name))
+	# Add Median column set to null value for years 1960, 1970, 1980 (where Median
+	# not included in census data)
+	if year > 1959 and year < 1981:
+		c.execute("ALTER TABLE census_{} add Median float8;".format(year))
+		c.execute("UPDATE census_{} SET Median = 'NaN';".format(year))
+
+	# Add standardized median column to census table
+	c.execute("create table intermed (gisjoin varchar(50), norm_med float8);")
+    add, insert, update = normalized_median(year)
+    c.execute(add)
+    c.execute(insert)
+    c.execute(update)
+    c.execute("drop table intermed;")
+
+
+conn.commit()
+c.close()
+conn.close()
---
+++ analysis/redline_boundaries.py
@@ -0,0 +1,62 @@
+import psycopg2 
+import os 
+
+def holc_buffer(grade):
+	'''
+	For a given HOLC grade, creates a query to identify the borders where a holc 
+	area of the given grade touches a holc area of a different grade, and creates 
+	boundary buffer polygon representing a 1/4 mile buffer out from the border of 
+	the HOLC area of the given grade into the neighboring HOLC area of a 
+	different grade.
+
+	Inputs:
+		grade (str): string representing HOLC grade
+
+	Returns:
+		query (str): string with query to 
+	'''
+
+  	query = '''INSERT INTO redline_poly (gid, holc_grade_b, holc_grade_a, geom) 
+          SELECT b.gid, b.holc_grade as holc_a, a.holc_grade as holc_b, 
+          geometry(ST_Intersection(ST_Buffer(CAST(a.geom AS geography), 402), 
+          b.geom)) FROM redline a, redline b  
+          WHERE a.holc_grade = '{}' AND b.holc_grade != '{}' AND 
+          ST_Intersects(ST_Buffer(CAST(a.geom AS geography), 402), 
+          b.geom);'''.format(grade, grade)
+
+  	return query
+
+conn = psycopg2.connect(database="capp30122", user="alenastern", password='', 
+	host="localhost", port="5432")
+c = conn.cursor()
+
+#Import redline boundaries shapefile
+os.system("shp2pgsql -s 4326 HOLC_chicago.shp public.redline | psql -d capp30122") 
+
+
+holc_grade_list = ['A', 'B', 'C', 'D']
+
+#create table for redline boundary buffer polygons
+table = '''CREATE TABLE redline_poly (poly_id SERIAL PRIMARY KEY, gid int, 
+	holc_grade_b varchar(10), holc_grade_a varchar(10), geom geometry);'''
+
+c.execute(table)
+
+#create buffers for each holc grade type
+for grade in holc_grade_list:
+  insert = holc_buffer(grade)
+  c.execute(insert)
+
+# Add column noting the border grades noted 'own grade - neighbor grade'
+add_holc_bound = "ALTER TABLE redline_poly ADD COLUMN holc_bound varchar(10);"
+update_holc_bound = '''UPDATE redline_poly SET holc_bound= concat(holc_grade_b, 
+	'-', holc_grade_a);'''
+c.execute(add_holc_bound)
+c.execute(update_holc_bound)
+
+conn.commit()
+c.close()
+conn.close()
+
+
+
